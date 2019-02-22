e2dc28168b4a218dbd91a20b01d03c8cd4511771
Saleh <saleh@grasppe.com>
2019-02-22 10:07:00 -0500

    Cleanup modules/alpha/lib/scope.js

--- dist/modules.js
+++ dist/modules.js
@@ -17,6 +17,44 @@
 
 	const ResolvedPromise = Promise.resolve();
 
+	const GlobalScope =
+		(typeof self === 'object' && self && self.self) ||
+		(typeof global === 'object' && global && global.global) ||
+		(() => (0, eval)('this'))();
+
+	const globals = (({eval: $eval}) => ({
+		eval: $eval,
+	}))(GlobalScope);
+
+	const scope = freeze(setPrototypeOf({...globals}, GlobalScope));
+
+	const locals = {};
+
+	const ModuleScope = new Proxy(scope, {
+		get: (target, property, receiver) => {
+			if (property in globals) return globals[property];
+			const value = property in GlobalScope && typeof property === 'string' ? GlobalScope[property] : undefined;
+			if (value && typeof value === 'function') {
+				const local = locals[property];
+				const {proxy} =
+					(local && local.value === value && local) ||
+					(locals[property] = {
+						value,
+						proxy: new Proxy(value, {
+							construct: (constructor, argArray, newTarget) => Reflect.construct(value, argArray, newTarget),
+							apply: (method, thisArg, argArray) =>
+								thisArg == null || thisArg === receiver ? value(...argArray) : Reflect.apply(value, thisArg, argArray),
+						}),
+					});
+				return proxy;
+			}
+			return value;
+		},
+		set: (globals, property) => {
+			throw ReferenceError(`${property} is not defined`);
+		},
+	});
+
 	class ModuleNamespaces {
 		import(url) {
 			return (
@@ -286,45 +324,6 @@ ${code}
 	})();
 
 	setPrototypeOf(Module, new ModuleStrapper());
-
-	const GlobalScope =
-		(typeof self === 'object' && self && self.self) ||
-		(typeof global === 'object' && global && global.global) ||
-		(() => (0, eval)('this'))();
-
-	const globals = (({eval: $eval}) => ({
-		eval: $eval,
-		// Module,
-	}))(GlobalScope);
-
-	const scope = freeze(setPrototypeOf({...globals}, GlobalScope));
-
-	const locals = {};
-
-	const ModuleScope = new Proxy(scope, {
-		get: (target, property, receiver) => {
-			if (property in globals) return globals[property];
-			const value = property in GlobalScope && typeof property === 'string' ? GlobalScope[property] : undefined;
-			if (value && typeof value === 'function') {
-				const local = locals[property];
-				const {proxy} =
-					(local && local.value === value && local) ||
-					(locals[property] = {
-						value,
-						proxy: new Proxy(value, {
-							construct: (constructor, argArray, newTarget) => Reflect.construct(value, argArray, newTarget),
-							apply: (method, thisArg, argArray) =>
-								thisArg == null || thisArg === receiver ? value(...argArray) : Reflect.apply(value, thisArg, argArray),
-						}),
-					});
-				return proxy;
-			}
-			return value;
-		},
-		set: (globals, property) => {
-			throw ReferenceError(`${property} is not defined`);
-		},
-	});
 
 	GlobalScope.DynamicModules = {
 		ModuleScope,
--- dist/modules.mjs
+++ dist/modules.mjs
@@ -14,6 +14,44 @@ const copyProperty = (target, source, identifier, alias = identifier) =>
 
 const ResolvedPromise = Promise.resolve();
 
+const GlobalScope =
+	(typeof self === 'object' && self && self.self) ||
+	(typeof global === 'object' && global && global.global) ||
+	(() => (0, eval)('this'))();
+
+const globals = (({eval: $eval}) => ({
+	eval: $eval,
+}))(GlobalScope);
+
+const scope = freeze(setPrototypeOf({...globals}, GlobalScope));
+
+const locals = {};
+
+const ModuleScope = new Proxy(scope, {
+	get: (target, property, receiver) => {
+		if (property in globals) return globals[property];
+		const value = property in GlobalScope && typeof property === 'string' ? GlobalScope[property] : undefined;
+		if (value && typeof value === 'function') {
+			const local = locals[property];
+			const {proxy} =
+				(local && local.value === value && local) ||
+				(locals[property] = {
+					value,
+					proxy: new Proxy(value, {
+						construct: (constructor, argArray, newTarget) => Reflect.construct(value, argArray, newTarget),
+						apply: (method, thisArg, argArray) =>
+							thisArg == null || thisArg === receiver ? value(...argArray) : Reflect.apply(value, thisArg, argArray),
+					}),
+				});
+			return proxy;
+		}
+		return value;
+	},
+	set: (globals, property) => {
+		throw ReferenceError(`${property} is not defined`);
+	},
+});
+
 class ModuleNamespaces {
 	import(url) {
 		return (
@@ -283,45 +321,6 @@ Module.debugging = (() => {
 })();
 
 setPrototypeOf(Module, new ModuleStrapper());
-
-const GlobalScope =
-	(typeof self === 'object' && self && self.self) ||
-	(typeof global === 'object' && global && global.global) ||
-	(() => (0, eval)('this'))();
-
-const globals = (({eval: $eval}) => ({
-	eval: $eval,
-	// Module,
-}))(GlobalScope);
-
-const scope = freeze(setPrototypeOf({...globals}, GlobalScope));
-
-const locals = {};
-
-const ModuleScope = new Proxy(scope, {
-	get: (target, property, receiver) => {
-		if (property in globals) return globals[property];
-		const value = property in GlobalScope && typeof property === 'string' ? GlobalScope[property] : undefined;
-		if (value && typeof value === 'function') {
-			const local = locals[property];
-			const {proxy} =
-				(local && local.value === value && local) ||
-				(locals[property] = {
-					value,
-					proxy: new Proxy(value, {
-						construct: (constructor, argArray, newTarget) => Reflect.construct(value, argArray, newTarget),
-						apply: (method, thisArg, argArray) =>
-							thisArg == null || thisArg === receiver ? value(...argArray) : Reflect.apply(value, thisArg, argArray),
-					}),
-				});
-			return proxy;
-		}
-		return value;
-	},
-	set: (globals, property) => {
-		throw ReferenceError(`${property} is not defined`);
-	},
-});
 
 GlobalScope.DynamicModules = {
 	ModuleScope,
