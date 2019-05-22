4979ffe899b551f3424090766c96a8746383c1a7
Saleh <saleh@grasppe.com>
2019-05-22 18:52:18 -0400

    Update modules.spec.js

--- modules.spec.js
+++ modules.spec.js
@@ -35,103 +35,119 @@
 			LEVEL >= 0 &&
 				new Module(
 					'level-0/module-scope',
-					(module, exports) => {
-						let temp;
-						const LOCAL_CONSTANT = 1;
-						module.await = (async () => {
-							await test(() => ({this: this, arguments: arguments}));
-							await test(() => _an_undefined_variable_);
-							await test(() => document);
-							await test(() => (_an_undefined_variable_ = 1));
-							await test(() => typeof _an_undefined_variable_);
-							await test(() => new Object({a: 1}));
-							// This ones causes a Proxy/inspect related error for some reason
-							await test(() => (([Object, temp = Object, Object] = [undefined, undefined, Object]), temp));
-							await test(() => LOCAL_CONSTANT === eval('LOCAL_CONSTANT'));
-							await test(() => typeof Object);
-							await test(() => Array(Object({a: String(1)})));
-							await test(() => new Array(new String('a'), new Number(2), Promise.resolve(Error('Not an Error!'))));
-							await test(() => ['a', 2, Error('Not an Error!')]);
-							await test(() => new Promise(setTimeout));
-						})();
-					},
+					module =>
+						void (() => {
+							let temp;
+							const LOCAL_CONSTANT = 1;
+							module.await(async () => {
+								await test(() => ({this: this, arguments: arguments}));
+								await test(() => _an_undefined_variable_);
+								await test(() => document);
+								await test(() => (_an_undefined_variable_ = 1));
+								await test(() => typeof _an_undefined_variable_);
+								await test(() => new Object({a: 1}));
+								// This ones causes a Proxy/inspect related error for some reason
+								await test(() => (([Object, temp = Object, Object] = [undefined, undefined, Object]), temp));
+								await test(() => LOCAL_CONSTANT === eval('LOCAL_CONSTANT'));
+								await test(() => typeof Object);
+								await test(() => Array(Object({a: String(1)})));
+								await test(() => new Array(new String('a'), new Number(2), Promise.resolve(Error('Not an Error!'))));
+								await test(() => ['a', 2, Error('Not an Error!')]);
+								await test(() => new Promise(setTimeout));
+							})();
+						}),
 					ModuleScope,
 				);
 
 			LEVEL >= 1 &&
 				new Module(
 					'level-1/global',
-					(module, exports) => {
-						`export { Object, Array }`;
-						let Object, Array;
-						exports.default = {Object, Array} =
-							('object' === typeof globalThis && globalThis && globalThis.globalThis === globalThis && globalThis) ||
-							('object' === typeof self && self && self.self === self && self) ||
-							('object' === typeof global && global && global.global === global && global) ||
-							('function' === typeof eval && (1, eval)('this')) ||
-							{};
-					},
-					ModuleScope,
-				);
-
-			LEVEL >= 1 &&
-				new Module(
-					'level-1/direct-exports',
-					(module, exports) => {
-						`export { q, TWO, y, g1, g2, g3, G1 }`;
-						const defaults = new class Defaults {}();
-						var q;
-						const TWO = 2;
-						let {y = {}} = defaults;
-						function g1() {}
-						async function g2() {}
-						function* g3() {}
-						class G1 {}
-						exports.default = defaults;
-					},
+					module =>
+						void (() => {
+							module.export`{ Object, Array }`;
+							let Object, Array;
+							module.export.default = {Object, Array} = globalThis;
+						}),
 					ModuleScope,
 				);
 
 			LEVEL >= 2 &&
 				new Module(
 					'level-2/global',
-					(module, exports) => {
-						`import global from '../level-1/global'`;
-						`import {Object, Array} from '../level-1/global'`;
-						`import * as global_namespace from '../level-1/global'`;
-						module.await = (async () => {
-							await test(() => [Object, global.Object]);
-							await test(() => `${global_namespace.default}`);
-						})();
-					},
+					module =>
+						void (() => {
+							module.import`global from '../level-1/global'`;
+							module.import`{Object, Array} from '../level-1/global'`;
+							module.import`* as global_namespace from '../level-1/global'`;
+							module.await(async () => {
+								await test(() => [Object, global.Object]);
+								await test(() => `${global_namespace.default}`);
+							})();
+						}),
+					ModuleScope,
+				);
+
+			LEVEL >= 1 &&
+				new Module(
+					'level-1/direct-exports',
+					module =>
+						void (() => {
+							module.export`{ q, TWO, y, g1, g2, g3, G1 }`;
+							const defaults = new (class Defaults {})();
+							var q;
+							const TWO = 2;
+							let {y = {}} = defaults;
+							function g1() {}
+							async function g2() {}
+							function* g3() {}
+							class G1 {}
+							module.export.default = defaults;
+						}),
 					ModuleScope,
 				);
 
 			LEVEL >= 2 &&
 				new Module(
 					'level-2/direct-imports',
-					(module, exports) => {
-						`import direct_exports_default from '../level-1/direct-exports'`;
-						`import * as direct_exports from '../level-1/direct-exports'`;
-						`import {g1, g2} from '../level-1/direct-exports'`;
-						`export { $g1, g2 as $g2 }`;
-						const $g1 = g1;
-						exports.default = {g1, g2, direct_exports_default, direct_exports};
-					},
+					module =>
+						void (() => {
+							module.import`direct_exports_default from '../level-1/direct-exports'`;
+							module.import`* as direct_exports from '../level-1/direct-exports'`;
+							module.import`{g1, g2} from '../level-1/direct-exports'`;
+							module.export`{ $g1, g2 as $g2 }`;
+							const $g1 = g1;
+							module.export.default = {g1, g2, direct_exports_default, direct_exports};
+						}),
+					ModuleScope,
+				);
+
+			LEVEL >= 2 &&
+				new Module(
+					'level-2/direct-imports',
+					module =>
+						void (() => {
+							module.import`direct_exports_default from '../level-1/direct-exports'`;
+							module.import`* as direct_exports from '../level-1/direct-exports'`;
+							module.import`{g1, g2} from '../level-1/direct-exports'`;
+							module.export`{ $g1, g2 as $g2 }`;
+							const $g1 = g1;
+							module.export.default = {g1, g2, direct_exports_default, direct_exports};
+						}),
 					ModuleScope,
 				);
 
 			LEVEL >= 2 &&
 				new Module(
 					'level-2/indirect-exports',
-					(module, exports) => {
-						`export { g1 as export_g1, g2 as export_g2, default as export_direct_exports_default } from '../level-1/direct-exports'`;
-						`export {default as export_direct_imports_default } from '../level-1/direct-exports'`;
-						`import * as direct_exports from '../level-1/direct-exports'`;
-						`import * as direct_imports from './direct-imports'`;
-						console.trace('indirect-exports');
-						exports.default = {direct_imports, direct_exports};
-					},
+					module =>
+						void (() => {
+							module.export`{ g1 as export_g1, g2 as export_g2, default as export_direct_exports_default } from '../level-1/direct-exports'`;
+							module.export`{default as export_direct_imports_default } from '../level-1/direct-exports'`;
+							module.import`* as direct_exports from '../level-1/direct-exports'`;
+							module.import`* as direct_imports from './direct-imports'`;
+							console.trace('indirect-exports');
+							module.export.default = {direct_imports, direct_exports};
+						}),
 					ModuleScope,
 				);
 		}
