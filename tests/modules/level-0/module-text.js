774699b45157838e3b188176d52d6cdd1b783fa0
Saleh Abdel Motaal <saleh@smotaal.io>
2019-12-09 14:48:16 -0500

    Create module-text.js

---
+++ tests/modules/level-0/module-text.js
@@ -0,0 +1,14 @@
+﻿// Line comment
+/* Block comment */
+'string';
+`template ${'string'}`;
+/(regular)[…](expression)/;
+
+/*/ export  //
+{} /*/;
+/*/ export async function *x() {} /*/;
+/*/ export class X extends Object {} /*/;
+
+/*/export/*/ const x = 1;
+exports.default = function y() {};
+/*/ import 'level-0/module-text' /*/;
